6888bf20143f2197ad71c837ce36d3f68c8317a5
kareempersonal <153719345+kareempersonal@users.noreply.github.com>
2023-12-13 11:28:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const events = require("events");
 const mongodb = require("./mongodb");
 const winston = require("./winston");
-const path = require("path");
+const console_logger = require("./console");
 
 class Settings {
   // Options
   static settings = {
-    log_errors_to_winston: true,
-    log_errors_to_mongodb: true,
+    log_inputs_to_console: false,
+    log_inputs_to_winston: true,
+    log_inputs_to_mongodb: true,
     winston_logs_directory: undefined,
-    winston_winston_default_severety_level: "info",
+    winston_default_severety_level: "info",
     mongodb_uri: "mongodb://localhost:27017",
     password: "x",
     database: "audits",
@@ -24,10 +25,11 @@ class Settings {
 
   set_settings(
     options = {
-      log_errors_to_winston: true,
-      log_errors_to_mongodb: true,
+      log_inputs_to_console: false,
+      log_inputs_to_winston: true,
+      log_inputs_to_mongodb: true,
       winston_logs_directory: undefined,
-      winston_winston_default_severety_level: "info",
+      winston_default_severety_level: "info",
       mongodb_uri: "mongodb://localhost:27017",
       password: "x",
       database: "audits",
@@ -46,7 +48,7 @@ const eventEmitter = new events.EventEmitter();
 const time_now = () => new Date().toISOString();
 
 eventEmitter.on("audit_to_mongodb", async (input, collection) => {
-  if (Settings.settings.log_errors_to_mongodb) {
+  if (Settings.settings.log_inputs_to_mongodb) {
     let data = {
       time: time_now(),
       ...input,
@@ -63,7 +65,7 @@ eventEmitter.on("audit_to_mongodb", async (input, collection) => {
 });
 
 eventEmitter.on("audit_to_winston", async (input, collection) => {
-  if (Settings.settings.log_errors_to_winston) {
+  if (Settings.settings.log_inputs_to_winston) {
     const message = `${JSON.stringify(input)}`;
     await winston(
       message,
@@ -75,18 +77,29 @@ eventEmitter.on("audit_to_winston", async (input, collection) => {
 
 const custom_audit = async (input = {}, severity_level, collection) => {
   if (typeof input === "object") {
-    if (Settings.settings.log_errors_to_mongodb) {
+    if (Settings.settings.log_inputs_to_mongodb) {
       eventEmitter.emit("audit_to_mongodb", input, collection);
     }
-    if (Settings.settings.log_errors_to_winston) {
+    if (Settings.settings.log_inputs_to_winston) {
       eventEmitter.emit("audit_to_winston", input, severity_level);
     }
   }
-
   if (typeof input === "string") {
-    if (Settings.settings.log_errors_to_winston) {
+    if (Settings.settings.log_inputs_to_winston) {
       eventEmitter.emit("audit_to_winston", input, severity_level);
     }
+  }
+  if (Settings.settings.log_inputs_to_console) {
+    await console_logger({
+      log_type:
+        severity_level ||
+        collection ||
+        Settings.settings.winston_default_severety_level,
+
+      message: input,
+
+      time: time_now(),
+    });
   }
 };
 
